adbd6b6fd347358385ce4121c8802b1e18e0b252
Pedro Santos <pedrosantosdevelop@gmail.com>
2023-07-09 19:54:18 -0300

    avaliacao autofill funcionando

--- src/routes/auth.py
+++ src/routes/auth.py
@@ -1,53 +1,76 @@
-from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask import (
+    Blueprint,
+    render_template,
+    request,
+    flash,
+    redirect,
+    url_for,
+    make_response,
+)
 from db import Database
 import base64
 
-blueprint = Blueprint('auth', __name__)
+blueprint = Blueprint("auth", __name__)
 
-@blueprint.route('/login', methods=['GET', 'POST'])
+
+@blueprint.route("/login", methods=["GET", "POST"])
 def login():
-    if request.method == 'POST':
-        matricula = request.form['matricula']
-        senha = request.form['senha']
+    if request.method == "POST":
+        matricula = request.form["matricula"]
+        senha = request.form["senha"]
 
         db = Database()
-        user = db.execute_fetchone_query("""
+        user = db.execute_fetchone_query(
+            """
             SELECT matricula, senha FROM estudante
             WHERE matricula = %s AND senha = %s
-        """, (matricula, senha))
+        """,
+            (matricula, senha),
+        )
 
         if user:
-            flash('Login realizado com sucesso!', 'success')
-            return redirect(url_for('home'))
+            flash("Login realizado com sucesso!", "success")
+            response = make_response(redirect(url_for("avaliacao.index")))
+            response.set_cookie("user", matricula, samesite="Lax")
+            return redirect(url_for("avaliacao.index"))
         else:
-            flash('Usuário ou senha incorretos!', 'warning')
+            flash("Usuário ou senha incorretos!", "warning")
 
-    return render_template('login.html')
+    return render_template("login.html")
 
-@blueprint.route('/register', methods=['GET', 'POST'])
+
+@blueprint.route("/register", methods=["GET", "POST"])
 def register():
-    if request.method == 'POST':
+    if request.method == "POST":
         student = dict(request.form)
 
-        student['email'] = f'{student["matricula"]}@aluno.unb.br'
+        student["email"] = f'{student["matricula"]}@aluno.unb.br'
 
-        avatar = request.files.get('avatar', False)
+        avatar = request.files.get("avatar", False)
 
         if avatar:
             encoded_image = base64.b64encode(avatar.read())
-            student['avatar'] = encoded_image
+            student["avatar"] = encoded_image
         else:
-            flash('Não foi possível carregar sua foto de perfil.', 'info')
+            flash("Não foi possível carregar sua foto de perfil.", "info")
 
         # Stores on database
 
         db = Database()
-        db.execute_query("""
+        db.execute_query(
+            """
             INSERT INTO estudante (matricula, avatar, senha, email)
             VALUES (%s, %s, %s, %s)
-        """, (student.get('matricula'), student.get('avatar'), student.get('senha'), student.get('email')))
+        """,
+            (
+                student.get("matricula"),
+                student.get("avatar"),
+                student.get("senha"),
+                student.get("email"),
+            ),
+        )
 
-        flash('Cadastro realizado com sucesso!', 'success')
-        return redirect(url_for('home'))
+        flash("Cadastro realizado com sucesso!", "success")
+        return redirect(url_for("auth.login"))
 
-    return render_template('register.html')
+    return render_template("register.html")
--- src/routes/avaliacao.py
+++ src/routes/avaliacao.py
@@ -1,19 +1,51 @@
-from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask import Blueprint, render_template, request, flash, redirect, url_for, jsonify
 from db import Database
 import base64
 
-blueprint = Blueprint('avaliacao', __name__)
+blueprint = Blueprint("avaliacao", __name__)
 
-@blueprint.route('/')
+
+@blueprint.route("/")
 def index():
     db = Database()
 
-    avaliacoes = db.execute_fetchall_query("""
+    avaliacoes = db.execute_fetchall_query(
+        """
         SELECT * FROM turma_avaliacoes_view
-    """)
+    """
+    )
 
-    return render_template('listar_avaliacoes.html', avaliacoes=avaliacoes)
+    return render_template("listar_avaliacoes.html", avaliacoes=avaliacoes)
 
-@blueprint.route('/criar_avaliacao')
-def create():
-    return render_template('criar_avaliacao.html')
+
+@blueprint.route(
+    "/criar_avaliacao/disciplina/<disciplina_codigo>/professor/<professor_matricula>/periodo/<periodo>",
+    methods=["GET", "POST"],
+)
+def create(disciplina_codigo: str, professor_matricula: str, periodo: str):
+    if request.method == "POST":
+        pass
+
+    current_matricula = request.cookies.get("user")
+
+    default_information = {
+        "disciplina_codigo": disciplina_codigo,
+        "professor_matricula": professor_matricula,
+        "periodo": periodo,
+        "estudante_matricula": current_matricula,
+    }
+
+    db = Database()
+
+    turma = db.execute_fetchone_query(
+        """
+        select p.nome as professor_nome, d.nome as disciplina_nome, t.periodo from turma t
+        left join disciplina d on d.codigo = t.codigo_disciplina
+        left join professor p on p.matricula = t.matricula_professor
+        where t.periodo = %s and t.matricula_professor = %s and t.codigo_disciplina = %s
+    """,
+        (periodo, professor_matricula, disciplina_codigo),
+    )
+    turma = {**dict(turma), **default_information}
+
+    return render_template("criar_avaliacao.html", turma=turma)
